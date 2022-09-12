0db2fea433981e37e63d842f04a28cdfc7278c85
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-12 16:18:48 +0100

    Update head.php

--- view/includes/head.php
+++ view/includes/head.php
@@ -1,1 +1,5 @@
-
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible">
+<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+<title>Online Store</title>
+<link rel="stylesheet" href="../style.css" type="text/css">
