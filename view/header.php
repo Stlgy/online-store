a1883f547e48f1121ee982667d36d28d11bf56ae
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-12 16:17:28 +0100

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,23 +1,16 @@
-<?php
-    session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stigy's Online Store</title>
-    <link rel="stylesheet" href="./style.css" type="text/css">
-</head>
-<body>
+<header>
     <nav>
-            <a href="index.php"><li>Home</li></a>
-            <?php if(!isset($_SESSION['username'])) : ?>
-                <a href="view/signup.php"><li>Sign Up</li></a>
-                <a href="view/login.php"><li>Login</li></a>
-            <?php else: ?>
-                <a href="./controllers/users.php?q=logout"><li>Logout</li></a>
-            <?php endif; ?>
+        <ul class="menu-main">
+            <li><a href="index.php">Home </a></li>
+            <li><a href="about.php">About </a></li>
+            <?php
+                //$_SESSION['username'] = 1;
+                if (!isset($_SESSION['username'])) { //codigo para caso user n logado
+                    echo '<li><a class="logs" href="signup.php">Sign Up</a></li>
+                          <li><a class="logs" href="login.php">Login</a></li>';
+                }else echo '<li><a href="../controllers/users.php?q=logout">Logout</a></li>';
+            ?>
         </ul>
     </nav>
+</header>
+    
