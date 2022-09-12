a4e04bf015e993c6243426359aac924194cd9f72
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-12 16:22:11 +0100

    Update login.php

--- view/login.php
+++ view/login.php
@@ -1,20 +1,29 @@
 <?php
-    include_once './view/header.php';
-    include_once './helpers/session-helper.php';
+    include_once 'includes/start.php';
+    include_once '../helpers/session_helper.php';
     
 ?>
-            <h1 class="header">Login</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include_once "includes/head.php";?>
+    </head>
+    <body>
+        <?php include_once "includes/header.php";?>
 
-            <?php flash('login')?>
+        <h1 class="header">Login</h1>
 
-            <form action="./controllers/users.php" method="post">
+            <?php flash('login');?>
+
+            <form action="../controllers/users.php" method="post">
                 <input type="hidden" name="type" value="login">
                 <input type="text" name="name/email" placeholder="Username/Email">
                 <input type="password" name="pwd" placeholder="Password">
-                <button type="submit" name="submit">Log in</button>
+                <button type="submit" name="submit">LOGIN</button>
             </form>
 
-            <div class="form-msg"><a href="./reset-password.php">Forgot Password?</a></div>
-<?php
-    include_once 'view/footer.php';
-?>
+            <div class="form-msg"><a href="../reset-password.php">Forgot Password?</a></div>
+
+        <?php include_once "includes/footer.php";?>
+    </body>
+</html>
