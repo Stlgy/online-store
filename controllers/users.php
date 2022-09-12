0aa14cfe5bd2e206e6964526d9c9a504e49e4971
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-12 16:26:26 +0100

    Update users.php

--- controllers/users.php
+++ controllers/users.php
@@ -1,7 +1,11 @@
 <?php
 
+<?php
+
 require_once '../models/user.php';
 require_once '../helpers/session_helper.php';
+
+//phpinfo();
 
 class Users{
 
@@ -21,7 +25,7 @@ class Users{
         $data = [
             'firstname' => trim($_POST['firstname']),
             'lastname'  => trim($_POST['lastname']),
-            'userid'    => trim($_POST['username']),
+            'username'    => trim($_POST['username']),
             'email'     => trim($_POST['email']),
             'pwd'       => trim($_POST['pwd']),
             'pwdrepeat' => trim($_POST['pwdrepeat'])
@@ -30,35 +34,35 @@ class Users{
         if(empty($data['firstname']) || empty($data['lastname']) || empty($data['username']) ||
         empty($data['email']) || empty($data['pwd']) || empty($data['pwdrepeat'])){
             flash("register", "Please fill out all fields");//assign error
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
         if(!preg_match("/^[a-zA-Z0-9]*$/", $data['firstname'])){
             flash("register", "Invalid firstname");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
         if(!preg_match("/^[a-zA-Z0-9]*$/", $data['lastname'])){
             flash("register", "Invalid lastname");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
         if(!preg_match("/^[a-zA-Z0-9]*$/", $data['username'])){
             flash("register", "Invalid username");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
         if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
             flash("register", "Invalid email");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
         if(strlen($data['pwd']) < 6){
             flash("register", "Invalid password");
-            redirect("view/signup.php");
-        }else if($data['pwd'] !== $data['pwdRepeat']){
+            redirect("../view/signup.php");
+        }else if($data['pwd'] !== $data['pwdrepeat']){
             flash("register", "No matching passwords");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
         }
        //email or username already exists
        if($this->userModel->findUserEmailUsername($data['email'], $data['username'])){
             flash("register", "Username or email already exist");
-            redirect("view/signup.php");
+            redirect("../view/signup.php");
        }
        //All validation checked
        //pwd hash 
@@ -66,10 +70,11 @@ class Users{
 
        //Register user
        if($this->userModel->register($data)){
-        redirect("view/login.php");//sent to login pag
+        redirect("../view/login.php");//sent to login pag
        }else{//stop script
             die("Something went wrong");
        }
+
     }
     public function login(){
         //sanatizing Post data
@@ -82,7 +87,7 @@ class Users{
         ];
         if(empty($data['name']) || empty($data['pwd'])){
             flash("login", "Please fill out all fields");//assign error
-            redirect("view/login.php");
+            redirect("../view/login.php");
             exit();
         }
         //Check for user  or email
@@ -94,11 +99,11 @@ class Users{
                 $this->createSession($loggedInUser);
             }else{
                 flash("login", "Incorrect password");
-                redirect("view/login.php");
+                redirect("../view/login.php");
             }
         }else{
             flash("login", "No user found ");
-            redirect("view/login.php");
+            redirect("../view/login.php");
         }
     }
     public function createSession($user){
