b24c5eaf8efb5b010161c630d4a725c4d248a3d4
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-13 11:45:04 +0100

    Create start.php

---
+++ app/view/includes/start.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    set_time_limit(0);
+    ini_set("display_errors", "On"); //faz com que o PHP emita todos os erros que existam durante a execução do script
+    ini_set("display_startup_errors", "On"); //faz com que o PHP emita todos os erros que estejam a impedir a execução do script
+    error_reporting(E_ALL); //ativar a emissão de todo o tipo de mensagens de aviso e erros.
+    define('BDS', 'localhost');
+    define('BDN', 'ifreshhost15_estagio');
+    define('BDU', 'ifreshhost15_estagio');
+    define('BDP', 'agosto2022#');
+    define('BDPX', 'exportador');
+    define('IDIOMA', "pt");
+?>
