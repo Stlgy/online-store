8b7ee3cd60a1834aee4e6c4535d0001e4550d804
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-14 11:55:59 +0100

    Update user.php

--- app/models/user.php
+++ app/models/user.php
@@ -1,4 +1,4 @@
-<?php
+?php
     require_once '../libraries/start.php';
     
     include('../libraries/class_utils.php');
@@ -6,15 +6,21 @@
     class User extends sys_utils{
 
         //Find user by existing email or username
-        public function findUsername($email, $username)
-        {
-            $res = SQL::run("SELECT * FROM " . BDPX . "_users WHERE username = ".$username." OR email = ".$email);
-            var_dump($res);
-            if(is_array($res) && count($res) > 0){
-                return $res;
-            }else{
-                return false;
+        public function findUsername($array, $modo) {
+            $result=[];
+
+            if($modo=="signup"){
+                $res = SQL::run("SELECT * FROM " . BDPX . "_users WHERE email  = ".$array[0]." OR username = ".$array[1]);
+            }else{//modo signup
+                $res = SQL::run("SELECT * FROM " . BDPX . "_users WHERE email  = ".$array[0]." OR username = ".$array[0]);
             }
+            
+            //var_dump($res);
+            if(is_array($res) && count($res) > 0){
+                $result= $res;
+            }
+
+            return $result;
         }
          //Register User
         public function register($data) :bool
@@ -29,12 +35,12 @@
             }
         }
         //Login User
-        public function login($nameEmail, $password)
+        public function login($name, $password)
         {
-            $row = $this->findUsername($nameEmail, $nameEmail);
-            if($row == false) return false;// error 
+            $row = $this->findUsername([$name], "login");
+            if(count($row)==0) return false;// error 
             //if found user
-            $hashedPassword = $row->password;
+            $hashedPassword = $row['pwd']->pwd;
             if(password_verify($password, $hashedPassword)){
                 return $row;
             }else{
