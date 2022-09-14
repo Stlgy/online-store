9e8b420613bbf495164a52e97f33f7f100b07847
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-14 11:54:40 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,21 @@
 <?php
-    include_once 'app/libraries/start.php';
-    include_once 'app/helpers/session_helper.php';
+    include_once 'libraries/start.php';
+    include_once 'helpers/session_helper.php';
     
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include_once "app/libraries/head.php";?>
+        <?php include_once "libraries/head.php";?>
     </head>
     <body>
-        <?php include_once "app/libraries/header.php";?>
+        <?php include_once "libraries/header.php";?>
 
         <h1 class="header">Login</h1>
 
             <?php flash('login');?>
 
-            <form class="formlogin" action="app/controllers/users.php" method="post">
+            <form class="formlogin" action="controllers/users.php" method="post">
                 <input type="hidden" name="type" value="login">
                 <input type="text" name="username" placeholder="Username/Email">
                 <input type="password" name="pwd" placeholder="Password">
@@ -24,6 +24,6 @@
 
             <div class="form-msg"><a href="reset-password.php">Forgot Password?</a></div>
 
-        <?php include_once "app/libraries/footer.php";?>
+        <?php include_once "libraries/footer.php";?>
     </body>
 </html>
