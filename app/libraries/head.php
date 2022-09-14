1cfee8a18b33f6e0a0d17e10400fd6b888722d83
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-14 11:57:09 +0100

    Update head.php

--- app/libraries/head.php
+++ app/libraries/head.php
@@ -1,6 +1,8 @@
-<?php ?>
+<?php 
+//echo realpath('.');
+?>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible">
 <meta name= "viewport" content="width=device-width, initial-scale=1.0">
 <title>Online Store</title>
-<link rel="stylesheet" href="app/public/css/style.css" type="text/css">
+<link rel="stylesheet" href="../public/css/style.css" type="text/css">
