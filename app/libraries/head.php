e3555bb71c9d66a11790e921d7d8a28249ae5a6a
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-16 16:48:19 +0100

    Update head.php

--- app/libraries/head.php
+++ app/libraries/head.php
@@ -4,5 +4,6 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible">
 <meta name= "viewport" content="width=device-width, initial-scale=1.0">
-<title>Online Store</title>
-<link rel="stylesheet" href="../public/css/style.css" type="text/css">
+<title>SHOP</title>
+<link rel="stylesheet" href="public/css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
