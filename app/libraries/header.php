032c438854b7469dc5c9c7b91bdcf475cce2cbd0
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-16 16:49:53 +0100

    Update header.php

--- app/libraries/header.php
+++ app/libraries/header.php
@@ -1,19 +1,33 @@
-<?php ?>
+<?php
+
+?>
 <header>
     <nav>
         <div>
             <ul class="menu-main">
-                <li><a href="index.php">HOME </a></li>
-                <li><a href="about.php">ABOUT </a></li>
+                <?php 
+                    if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+                        ?>
+                        <h1 id="Welcome">welcome <?= $_SESSION['username']; ?></h1>
+                        <?php
+                        
+                    }
+                ?>
+                <li><a href="index.php"><i class="bi bi-house-fill"></i></a></li>
+                <li><a href="about.php"><i class="bi bi-info-circle-fill"></i></a></li>
+                <li><a href="about.php"><i class="bi bi-shop-window"></i></a></li>
                 <?php
-                    //$_SESSION['username'] = 1;
-                    if (!isset($_SESSION['username'])) { //codigo para caso user n logado
-                        echo '
-                            <ul class="menu-member">
+                    //$_SESSION['username'] = 0;
+                    if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+                        ?>
                             <li><a class="logs" href="signup.php">SIGN UP</a></li>
-                            <li><a class="logs" href="login.php">LOGIN</a></li>';
-                    }else 
-                        echo '<li><a href="../controllers/users.php?q=logout">Logout</a></li>';
+                            <li><a class="logs" href="login.php">LOGIN</a></li>
+                        <?php
+                    }else{
+                        ?>
+                            <li><a class="logs" href="controllers/users.php?q=logout">Logout</a></li>
+                        <?php
+                    }
                 ?>
             </ul>
         </div>
