d6e10693fbc27a9429b489ae6dcdb9860a0d7d22
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-16 12:33:56 +0100

    Update login.php

--- login.php
+++ login.php
@@ -16,14 +16,15 @@
             <?php flash('login');?>
 
             <form class="formlogin" action="controllers/users.php" method="post">
+            
                 <input type="hidden" name="type" value="login">
                 <input type="text" name="username" placeholder="Username/Email">
                 <input type="password" name="pwd" placeholder="Password">
-                <button type="submit" name="submit">LOGIN</button>
+                <button type="submit" name="login-btn">LOGIN</button>
+                <div class="form-msg">Not yet a member?<a href="signup.php">Sign Up</a></div>
+                <div><a href="reset-password.php">Forgot Password?</a></div>
             </form>
-
-            <div class="form-msg"><a href="reset-password.php">Forgot Password?</a></div>
-
+         
         <?php include_once "libraries/footer.php";?>
     </body>
 </html>
