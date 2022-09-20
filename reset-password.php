06fcab5ef16bd8348684a986b837b1f036aa3f27
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-20 16:02:18 +0100

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -20,15 +20,24 @@ include_once 'helpers/session_helper.php';
             <div class="col-md-8 col-xs-12 col-sm-12 resetpwd__form ">
                 <div class="container-fluid">
                     <div class="row">
-                        <h2>Recover Password</h2>
+                        <h2>Reset your password</h2><br>
+                        <p>An e-mail will be send to you with instructions on how to reset your password</p>
                         <?php flash('reset'); ?>
                     </div>
                     <div class="row">
                         <form class="form__resetpwd" action="controllers/users.php" method="post">
                             <input type="hidden" name="type" value="resetPwd">
                             <div class="row">
-                                <input type="text" name="email" class="form__resetpwd" placeholder="Email">
+                                <input type="text" name="email" class="form__resetpwd" placeholder="Enter your e-mail address">
                             </div>
+                        </form>
+                        <?php
+                            if(isset($_GET["forgot-pwd"])){
+                                if($_GET["forgot-pwd"] == "success"){
+                                    echo '<p class="signupsuccess">Check your e-mail</p>';
+                                }
+                            }
+                        ?>
                     </div>
                     <div class="row-submit">
                         <button type="submit" class="btn" name="forgot-pwd">
@@ -46,4 +55,7 @@ include_once 'helpers/session_helper.php';
     </div>
 </body>
 <html>
+
+
+
 
