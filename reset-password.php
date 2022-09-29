d258dc5c51470191cd57aeb8ed669434cc8fbd6b
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-09-29 15:29:59 +0100

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -8,54 +8,38 @@ include_once 'helpers/session_helper.php';
 
 <head>
     <?php include_once "libraries/head.php"; ?>
+    <link rel="stylesheet" href="public/css/style_log.css" type="text/css">
 </head>
 
 <body>
     <?php include_once "libraries/header.php"; ?>
-    <link rel="stylesheet" href="public/css/style_log.css" type="text/css">
 
-
-    <div class="container-fluid">
-        <div class="row main-content bg-sucess text-ceter">
-            <div class="col-md-8 col-xs-12 col-sm-12 resetpwd__form ">
+    <div class="container-center">
+        <div class="row main-content text-center">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 resetpwd__form ">
                 <div class="container-fluid">
-                    <div class="row">
-                        <h2>Reset your password</h2><br>
-                        <p>An e-mail will be send to you with instructions on how to reset your password</p>
-                        <?php flash('reset'); ?>
-                    </div>
-                    <div class="row">
-                        <form class="form__resetpwd" action="controllers/users.php" method="post">
-                            <input type="hidden" name="type" value="resetPwd">
-                            <div class="row">
-                                <input type="text" name="email" class="form__resetpwd" placeholder="Enter your e-mail address">
-                            </div>
-                        </form>
-                        <?php
-                            if(isset($_GET["forgot-pwd"])){
-                                if($_GET["forgot-pwd"] == "success"){
-                                    echo '<p class="signupsuccess">Check your e-mail</p>';
-                                }
-                            }
-                        ?>
-                    </div>
-                    <div class="row-submit">
-                        <button type="submit" class="btn" name="forgot-pwd">
-                            Receive Email
-                        </button>
-                    </div>
+
+                    <h2>Reset your password</h2><br>
+                    <p>An e-mail will be send to you with instructions on how to reset your password</p>
+
+                    <?php flash('reset'); ?>
+
+                    <form class="form__resetpwd1" action="controllers/users.php" method="post">
+                        <input type="hidden" name="type" value="send">
+                        <div class="row">
+                            <input type="text" name="email" class="form__input" placeholder="Enter your e-mail address">
+                        </div>
+                        <div class="row-submit">
+                            <button type="submit" class="btn" id="btns" name="reset-request-submit" >RECEIVE EMAIL</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    <div>
-        <!-- Footer -->
-        <!-- <div class="container-fluid text-center footer"> -->
-        <?php include_once "libraries/footer.php"; ?>
+    <!-- Footer -->
+    <!-- <div class="container-fluid text-center footer"> -->
+    <?php include_once "libraries/footer.php"; ?>
     </div>
 </body>
 <html>
-
-
-
-
