3aa284e0e83a04f207961b1f7bb0f14788be6a4e
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-04 11:43:13 +0100

    Update login.php

--- login.php
+++ login.php
@@ -15,21 +15,20 @@ include_once 'helpers/session_helper.php';
 
     <div class="container-center">
         <div class="row main-content text-center">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 login__form ">
-                <br>
+            <div class="col-12 login__form ">
+               
                 <h2>Log In</h2>
-                <br>
-                <?php flash('login'); ?>
+                 <br>
+                <?php flash('login'); ?> 
 
                 <form class="form__login" action="controllers/users.php" method="post">
                     <input type="hidden" name="type" value="login">
                     <div class="row">
-                        
-                        <input type="text" name="username" id="username" class="form__input " placeholder="Username/Email">
+                        <input type="text" name="username" id="username" class="form__input" placeholder="Username/Email">
                         <input type="password" name="pwd" id="password" class="form__input" placeholder="Password">
                     </div>
                     <div class="row-submit">
-                        <input type="submit" value="LOGIN" class="btn btn-pequeno">
+                        <input type="submit" value="LOGIN" class="btn">
                         <div class="">
                             <a href="reset-password.php" role="button" id="btns">RESET PASSWORD</a>
                         </div>
