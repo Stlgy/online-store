47f305e63393aa1d119221d9c993f158c0ac2c9d
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-07 11:13:21 +0100

    Create logout.php

---
+++ app/views/user/logout.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../../libraries/start.php';
+require_once '../../controllers/usersController.php'; 
+
+require_once('../../models/user.php');
+$logoutUser = $init->logout();
+
+
+?>
