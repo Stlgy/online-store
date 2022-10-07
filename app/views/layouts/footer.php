e8ab26cc71703387e5057c6db6f2e4d685ffebc1
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-07 11:17:52 +0100

    Create footer.php

---
+++ app/views/layouts/footer.php
@@ -0,0 +1,11 @@
+<?php ?>
+<footer>
+    <div id="footer">
+        <p>Online Store </p>
+        <ul>
+            <li>
+                <a href="contact.php">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</footer>
