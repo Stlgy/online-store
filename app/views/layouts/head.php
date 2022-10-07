dd162f7c316893a98d5a79fef72d69297a81aae8
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-07 11:18:12 +0100

    Update head.php

--- app/views/layouts/head.php
+++ app/views/layouts/head.php
@@ -1,1 +1,11 @@
+<?php 
+?>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible">
+<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+<title>SHOP</title>
+<base href="https://exportador.ifresh-host.eu/onlinestore/app/">
+<link rel="stylesheet" href="template/css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
