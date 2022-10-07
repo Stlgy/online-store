c75e249040ba70574e5af6d4a9df0aba1a083bb5
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-07 11:04:05 +0100

    Delete head.php

--- app/libraries/head.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//echo realpath('.');
-?>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible">
-<meta name= "viewport" content="width=device-width, initial-scale=1.0">
-<title>SHOP</title>
-<link rel="stylesheet" href="public/css/style.css" type="text/css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
